02145b52780fd6fa093daa1a2f05f4f7d5ffa8ff
Reggie Davis <reginald.davis88@gmail.com>
2016-06-01 17:08:33 -0500

    Styled Welcome Page & Navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
 
   resources :documents
 
   authenticated :user do
-    root "documents#index", as "authenticated_root"
+    root "documents#index", as: "authenticated_root"
   end
+
+  root 'welcome#index'
 
 end
